aed8b1eb148eb01c982dfada3b17b61e3f91bd77
Yanick <nckmuhire@gmail.com>
2025-10-24 07:51:11 +0200

    configure Django settings and add User/Review models

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    
+    def __str__(self):
+        return self.username
+
+class Review(models.Model):
+    RATING_CHOICES = [
+        (1, '1 Star'),
+        (2, '2 Stars'),
+        (3, '3 Stars'),
+        (4, '4 Stars'),
+        (5, '5 Stars'),
+    ]
+    
+    movie_title = models.CharField(max_length=255)
+    review_content = models.TextField()
+    rating = models.IntegerField(choices=RATING_CHOICES, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        unique_together = ['movie_title', 'user']  # One review per movie per user
+        ordering = ['-created_date']
+    
+    def __str__(self):
+        return f"{self.movie_title} - {self.user.username} - {self.rating}/5"
